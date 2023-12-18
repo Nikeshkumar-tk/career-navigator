1a1da53f51900ab83d8f6caca4b02a181668fbdc
Nikeshkumar-tk <104181447+Nikeshkumar-tk@users.noreply.github.com>
2023-12-18 17:07:51 +0530

    footer modified

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -3,16 +3,15 @@ import { Card, CardDescription, CardHeader } from "@/components/ui/card"
 import { Careers, Users } from "@/lib/mongoose";
 import { authOptions } from "@/lib/next-auth";
 import { getServerSession } from "next-auth";
-import { useQuery } from "react-query"
 
 
 export default async function CareerPage() {
     const session = await getServerSession(authOptions);
     const userInfo = await Users.findById(session?.user.sub)
     const careersInfo = await Careers.findOne({ course:userInfo.stream });
-    return (<div>
+    return (<div className="h-[80vh]">
         <h1 className="text-2xl font-mono">Careers recommended based on your education</h1>
-        <div className="mt-4">
+        <div className="mt-4 ">
             {JSON.parse(JSON.stringify(careersInfo)).careers.map((career: Record<string, string>, index: number) => (
                 <Card key={index} className="w-[20rem]">
                     <CardHeader>{career.name}
--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -15,90 +15,27 @@ export function SiteFooter() {
         <section
           id="footer-content"
           aria-labelledby="footer-content-heading"
-          className="flex flex-col gap-10 lg:flex-row lg:gap-20"
+          className="flex justify-between gap-10 lg:flex-row lg:gap-20"
         >
           <section
             id="footer-branding"
             aria-labelledby="footer-branding-heading"
           >
-            <Link href="/" className="flex w-fit items-center space-x-2">
+            <Link href="/" className="w-fit flex items-center space-x-2">
               <Icons.logo className="h-6 w-6" aria-hidden="true" />
               <span className="font-bold">{appConfig.name}</span>
               <span className="sr-only">Home</span>
             </Link>
           </section>
           <section
-            id="footer-links"
-            aria-labelledby="footer-links-heading"
-            className="grid flex-1 grid-cols-1 gap-10 xxs:grid-cols-2 sm:grid-cols-4"
-          >
-            {appConfig.footerNav.map((item) => (
-              <div key={item.title} className="space-y-3">
-                <h4 className="text-base font-medium">{item.title}</h4>
-                <ul className="space-y-2.5">
-                  {item.items.map((link) => (
-                    <li key={link.title}>
-                      <Link
-                        href={link.href}
-                        target={link?.external ? "_blank" : undefined}
-                        rel={link?.external ? "noreferrer" : undefined}
-                        className="text-sm text-muted-foreground transition-colors hover:text-foreground"
-                      >
-                        {link.title}
-                        <span className="sr-only">{link.title}</span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </section>
-          <section
             id="newsletter"
             aria-labelledby="newsletter-heading"
-            className="space-y-3"
+            className="space-y-3 self-end"
           >
-            <h4 className="text-base font-medium">
-              Subscribe to our newsletter
-            </h4>
-            <SubscribeToNewsletterForm />
+            <h4 className="text-base text-muted-foreground w-[25rem] font-medium">
+            Career Navigator is a comprehensive platform designed to guide students toward successful and independent career choices. By providing tailored assessments, personalized insights, and valuable resources, it empowers students to make informed decisions, aligning their skills and passions with diverse career opportunities for a fulfilling and prosperous future.            </h4>
+            {/* <SubscribeToNewsletterForm /> */}
           </section>
-        </section>
-        <section
-          id="footer-bottom"
-          aria-labelledby="footer-bottom-heading"
-          className="flex items-center space-x-4"
-        >
-          <div className="flex-1 text-left text-sm leading-loose text-muted-foreground">
-            Built by{" "}
-            <Link
-              href="https://twitter.com/sadmann17"
-              target="_blank"
-              rel="noreferrer"
-              className="font-semibold transition-colors hover:text-foreground"
-            >
-              Sadman
-              <span className="sr-only">Twitter</span>
-            </Link>
-            .
-          </div>
-          <div className="flex items-center space-x-1">
-            <Link
-              href={appConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className={cn(
-                buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })
-              )}
-            >
-              {/* <Icons.gitHub className="h-4 w-4" aria-hidden="true" /> */}
-              <span className="sr-only">GitHub</span>
-            </Link>
-            {/* <ModeToggle /> */}
-          </div>
         </section>
       </Shell>
     </footer>
